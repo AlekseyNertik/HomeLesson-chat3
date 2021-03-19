88e7f2882673d262d698d2b8150c5badd6565fa7
AleksyNetik <dusty70rus@gmail.com>
2021-03-19 22:06:23 +0700

    добавлена авторизация с помощью базы данных

--- src/server/SimpleAuthService.java
+++ src/server/SimpleAuthService.java
@@ -1,9 +1,32 @@
 package server;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SimpleAuthService implements AuthService{
+
+    private static Connection connection;
+    private static Statement stmt;
+    private static PreparedStatement pstmt;
+
+    public static void connect() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:main.db");
+            stmt = connection.createStatement();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void disconnect() {
+        try {
+            connection.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
 
     private class UserData {
         String login;
@@ -14,6 +37,7 @@ public class SimpleAuthService implements AuthService{
             this.login = login;
             this.password = password;
             this.nickName = nickName;
+
         }
     }
 
@@ -21,14 +45,51 @@ public class SimpleAuthService implements AuthService{
 
     public SimpleAuthService() {
         users = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            users.add(new UserData("login" + i, "pass" + i, "nick" + i));
-        }
-        users.add(new UserData("qwe", "qwe", "qwe"));
-        users.add(new UserData("asd", "asd", "asd"));
-        users.add(new UserData("zxc", "zxc", "zxc"));
-    }
+//========== Cоздаю таблицу базы данных ==  в sqlLite созданная таблица не подключается ========
+//        connect();
+//        try {
+//            stmt.executeUpdate("CREATE TABLE UserList1 (" +
+//                    " Num INTEGER PRIMARY KEY AUTOINCREMENT," +
+//                    " Login TEXT," +
+//                    " Password TEXT," +
+//                    " Nick TEXT)");
+//        } catch (SQLException throwables) {
+//            throwables.printStackTrace();
+//        }
+//        try {
+//            stmt.executeUpdate("INSERT INTO UserList1 (Num, Login, Password, Nick) " +
+//                    "VALUES (1, 'qwe', 'qwe', 'qwe')");
+//            stmt.executeUpdate("INSERT INTO UserList1 (Num, Login, Password, Nick) " +
+//                    "VALUES (2, 'asd', 'asd', 'asd')");
+//            stmt.executeUpdate("INSERT INTO UserList1 (Num, Login, Password, Nick) " +
+//                    "VALUES (3, 'zxc', 'zxc', 'zxc')");
+//        } catch (SQLException throwables) {
+//            throwables.printStackTrace();
+//        }
+//        disconnect();
+//    }
+//===============================================
 
+
+//        for (int i = 0; i < 10; i++) {
+//            users.add(new UserData("login" + i, "pass" + i, "nick" + i));
+//        }
+connect();
+        try {
+            ResultSet rs = stmt.executeQuery("SELECT * FROM UserList1");
+            while (rs.next()) {
+//                System.out.println(rs.getInt(1) + " " + rs.getString("name")
+//                        + " " + rs.getString("score"));
+            users.add(new UserData(rs.getString("Login"), rs.getString("Password"), rs.getString("Nick")));
+//            users.add(new UserData("asd", "asd", "asd"));
+//            users.add(new UserData("zxc", "zxc", "zxc"));
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+disconnect();
+    }
+//=================================================
     @Override
     public String getNickByLoginAndPassword(String login, String password) {
         for(UserData user : users){
